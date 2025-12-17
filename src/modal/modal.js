c24e8633ad602df659a8b09ee03a57c7d98deb3b
fysh711426 <fysh711426@hotmail.com>
2025-12-18 07:22:40 +0800

    add modal

---
+++ src/modal/modal.js
@@ -0,0 +1,50 @@
+var modal = (function () {
+    function createElement(html) {
+        var div = document.createElement('div');
+        div.innerHTML = html;
+        if (div.children.length > 0) {
+            return div.children[0];
+        }
+    }
+    function remove(element) {
+        try {
+            document.body.removeChild(element);
+        } catch { }
+    }
+    return function (template, _settings) {
+        var setting = {};
+        _settings = _settings || {};
+        
+        var ref = {
+            open: open,
+            close: close,
+            onOpened: null,
+            onClosed: null
+        };
+
+        var element = null;
+        function open() {
+            if (!element) {
+                var html = template.innerHTML;
+                element = createElement(html);
+                document.body.appendChild(element);
+                template.innerHTML = '';
+                if (ref.onOpened) {
+                    ref.onOpened();
+                }
+            }
+        }
+        function close() {
+            if (element) {
+                var html = element.outerHTML;
+                remove(element);
+                element = null;
+                template.innerHTML = html;
+                if (ref.onClosed) {
+                    ref.onClosed();
+                }
+            }
+        }
+        return ref;
+    }
+})();
